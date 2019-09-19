63ff398efa2e7240c2daab1f075ddb7b85806853
Matthaus Zillig <mathaus.zillig@corebiz.com.br>
2019-09-19 17:14:12 -0300

    created project with create react app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './homepage.component';
 import './App.css';
 
 function App() {
-  return (
-    <div >
-      Hello World
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
